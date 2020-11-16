814de560afb2dde0e4bf4c02863d9013913b247f
dayam <dayam@Xs-MacBook-Air.local>
2020-11-16 14:38:06 -0700

    Initial attempted and LD pruned GDS merge

---
+++ create_ld_pruned_gds.R
@@ -0,0 +1,50 @@
+#Set parameters
+args <- commandArgs(trailingOnly = TRUE)
+maf <- as.numeric(args[1])
+missing.rate <- as.numeric(args[2])
+slide.max.bp <- as.numeric(args[3])
+r2.ld.threshold <- as.numeric(args[4])
+subset.snp.ids <- read.delim(args[5], stringsAsFactors = F)[,1]
+gds.files <- c()
+
+#Load librariess
+library(SNPRelate)
+library(SeqArray)
+library(GWASTools)
+
+#Process GDS files
+gds.in.files <- scan(text=args[6], what="")
+for (gds.in.file in gds.in.files) {
+  #Setup for the file
+  gds.out.file <- paste0("pruned_", basename(gds.in.file))
+  print(paste("Processing", gds.in.file, "to", gds.out.file))
+  
+  #Get list of matching variant IDs
+  gds <- seqOpen(gds.in.file)
+  variant.ids <- seqGetData(gds, "variant.id")
+  chromosome <- seqGetData(gds, "chromosome")
+  position <- seqGetData(gds, "position")
+  alleles <- gsub(",", ":", seqGetData(gds, "allele"))
+  snp.ids <- paste(chromosome, position, alleles, sep=":")
+
+  #Perform SNP list, MAF and missing rate filter
+  snpset <- snpgdsSelectSNP(gds, snp.id=variant.ids[snp.ids %in% subset.snp.ids], maf=maf, missing.rate=missing.rate)
+  filtered <- unlist(snpset, use.names=FALSE)
+  
+  #Perform LD pruning
+  set.seed(100) 
+  snpset <- snpgdsLDpruning(gds, snp.id=filtered, method="corr", slide.max.bp=slide.max.bp, 
+                            ld.threshold=sqrt(r2.ld.threshold), verbose=FALSE)
+  pruned <- unlist(snpset, use.names=FALSE)
+  
+  #Subset the file
+  seqSetFilter(gds, variant.id=pruned)
+  seqGDS2SNP(gds, gds.out.file)
+  seqClose(gds)
+  gds.files <- c(gds.files, gds.out.file)
+}
+
+#Write the new combined pruned GDS files 
+snpgdsCombineGeno(gds.files, "combined_pruned.gds")
+
+
---
+++ get_ld_pruned_gds.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Set parameters
+maf=$1
+missing_rate=$2
+slide_max_bp=$3
+r2_ld_threshold=$4
+snp_id_file=$5
+
+#Call R script
+
+#cat /home/analyst/create_ld_pruned_gds.R | R --vanilla --args \
+cat create_ld_pruned_gds.R | R --vanilla --args \
+   $maf \
+   $missing_rate \
+   $slide_max_bp \
+   $r2_ld_threshold \
+   $snp_id_file \
+   "${*:6}"
