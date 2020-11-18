4b449418be1dc0226b82c2330a9a5fd6869337a7
dayam <dayam@Xs-MacBook-Air.local>
2020-11-18 06:16:05 -0700

    Added PCA from pruned GDS file

---
+++ GENESIS_PCAIR_PCRELATE_PRUNED_GDS.R
@@ -0,0 +1,47 @@
+#Set parameters
+args <- commandArgs(trailingOnly = TRUE)
+gds.fn <- args[1]
+out.file.prefix <- args[2]
+log.fn <- paste0(out.file.prefix, "_GENESIS_PCA.log")
+pca.rdata.out.fn <- paste0(out.file.prefix, "_pca.RData")
+km.rdata.out.fn <- paste0(out.file.prefix, "_km.RData")
+
+#Load librariess
+library(SNPRelate)
+library(GWASTools)
+library(GENESIS)
+
+#Open the log file
+sink(log.fn)
+
+#Get KING relatedness estimates
+gds <- snpgdsOpen(gds.fn)
+ibd <- snpgdsIBDKING(gds)
+colnames(ibd$kinship) <- ibd$sample.id
+rownames(ibd$kinship) <- ibd$sample.id
+
+#Close the GDS file so it can be read again for PC-AiR
+snpgdsClose(gds)
+
+#Run PC-AiR
+geno <- GdsGenotypeReader(filename=gds.fn)
+geno.data <- GenotypeData(geno)
+pca <- pcair(geno.data, 
+                       kinobj=ibd$kinship,
+                       divobj=ibd$kinship)
+
+
+#Run PC-Relate
+geno.data <- GenotypeBlockIterator(geno.data, snpInclude=pruned)
+pc.relate <- pcrelate(geno.data, pcs = pca$vectors[,1:nr.pcs], 
+                       training.set = pca$unrels)
+km <- pcrelateToMatrix(pc.relate)
+
+
+#Write PC-AiR output
+save(pca, file=pca.rdata.out.fn)
+save(km, file=km.rdata.out.fn)
+
+#Close the GDS file and log file
+close(geno.data)
+sink()
