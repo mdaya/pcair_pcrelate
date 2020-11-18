35fc785e41eb6b49c2148b2d936c87a327294b0b
dayam <dayam@Xs-MacBook-Air.local>
2020-11-18 06:35:47 -0700

    fixed parameters for get_high_rsq_ids.sh

--- get_high_rsq_ids.sh
+++ get_high_rsq_ids.sh
@@ -3,4 +3,4 @@
 snp_id_file=$1
 min_rsq=$2
 
-cat /home/analyst/get_high_rsq_ids.R | R --vanilla --args $snp_id_file $min_rsq $out_file_name "${*:3}"
+cat /home/analyst/get_high_rsq_ids.R | R --vanilla --args $snp_id_file $min_rsq "${*:3}"
